0cc774f15ccd5756e6c632ab9814e61ab2526272
Matthew MIller <mrm36@njit.edu>
2017-05-06 19:00:15 -0400

    Client Request to get the current package number for installation, updated installation script

--- installBackend.sh
+++ installBackend.sh
@@ -1,17 +1,16 @@
-echo "Unzippin Package... ;)"
+echo "Unzipping Package... ;)"
 # Getting current package Number
-packageNum=`php /home/matt/git/rabbitmqphp_example_deployTest/testRabbitMQClient.php currentPackage backendPackage.tar.gzi | xargs`
+packageNum=`php currentPackage.php | xargs`
 
-tar -xzf $HOME/Packages/backendPackage-v"$packageNum".tar.gz -C /home/uziar/Packages/backend/
+tar -xzf $HOME/Packages/backendPackage-v"$packageNum".tar.gz -C $HOME/Packages
 
 echo "Copying SQL Data..."
 echo "Enter MySQL  Password?..."
-	mysql -u root -p login < $HOME/Packages/backend/backendPackage-v"$packageNum"/mySqlData/backup_sql.sql
+	mysql -u root -p login < $HOME/Packages/backendPackage-v"$packageNum"/mySqlData/backup_sql.sql
 
-mkdir /home/uzair/Desktop/GoliathBackend
-
-echo "Installing backend.. how exciting -_-"
-mv $HOME/Packages/backend/backendPackage-v"$packageNum"/it1490_backend_final $HOME/Desktop/GoliathBackend/
+mkdir $HOME/Desktop/GoliathBackendServer
+	echo "Installing backend.. how exciting -_-"
+	mv $HOME/Packages/backendPackage-v"$packageNum"/it1490_backend_final $HOME/Desktop/GoliathBackend/
 
 echo "Backend Package Sucessfully Installed!"
 
